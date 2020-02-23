709fe665e189c26e2294b041121be3124fdf5d51
I Putu Gede Wirasuta <wirasutat@gmail.com>
2020-02-23 13:40:28 +0700

    config pwa

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,26 @@
 module.exports = {
   transpileDependencies: ['vuetify'],
   pwa: {
-    themeColor: '#FF5252'
+    themeColor: '#FF5252',
+    name: 'NIM Finder',
+    workboxPluginMode: 'GenerateSW',
+    workboxOptions: {
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('^https://nim.wirasuta.com/api/'),
+          handler: 'networkFirst',
+          options: {
+            networkTimeoutSeconds: 20,
+            cacheName: 'api',
+            cacheableResponse: {
+              statuses: [0, 200]
+            },
+            expiration: {
+              maxAgeSeconds: 60 * 60 * 24 * 90
+            }
+          }
+        }
+      ]
+    }
   }
 }
